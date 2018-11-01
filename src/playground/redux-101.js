cdeb766f76c5c19da627f7f7c79fdead0e33168f
Andres Mena <ballerlikemike23@gmail.com>
2018-10-31 23:01:35 -0400

    Subscribing and Dynamic Actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,18 @@ import { createStore } from 'redux'
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       }
     case 'DECREMENT':
+     const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
+      }
+    case 'SET':
+      return {
+        count: action.count
       }
     case 'RESET':
       return {
@@ -17,21 +23,16 @@ const store = createStore((state = { count: 0 }, action) => {
     default:
       return state
   }
-
-  // if (action.type === 'INCREMENT') {
-  //   return {
-  //     count: state.count + 1
-  //   }
-  // } else {
-  //   return state
-  // }
 })
 
-console.log(store.getState())
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState())
+})
 
 // I'd like to increment the count
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
 })
 
 store.dispatch({
@@ -48,5 +49,13 @@ store.dispatch({
   type: 'DECREMENT'
 })
 
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+})
 
-console.log(store.getState())
+// I'd like to set the count
+store.dispatch({
+  type: 'SET',
+  count: 101
+})
