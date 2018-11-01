e0f071eee370584d8cb05b466499cfbf14c6e41a
Andres Mena <ballerlikemike23@gmail.com>
2018-10-31 23:28:37 -0400

    ES6 Array Destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//////////////////////
+// Object Destructing
+//////////////////////
+
 // const person = {
 //   name: 'Andres',
 //   age: 27,
@@ -15,14 +19,30 @@
 // const { city, temp: temperature } = person.location
 // console.log(`It's ${temperature} in ${city}`)
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-}
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// }
 
-const { name: publisherName = 'Self-Published'} = book.publisher
+// const { name: publisherName = 'Self-Published'} = book.publisher
+// console.log(publisherName)
 
-console.log(publisherName)
+//////////////////////
+// Array Destructing
+//////////////////////
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147']
+
+// console.log(`You are in ${address[1]} ${address[2]}.`)
+
+// const [street, city, state, zip] = address
+const [, city, state] = address
+console.log(`You are in ${city}, ${state}.`)
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75']
+const [ coffee, , medium] = item
+
+console.log(`A medium ${coffee} costs ${medium}`)
