c27e26452a7f44128e0ad39755e2af379acc9191
Andres Mena <ballerlikemike23@gmail.com>
2018-11-03 19:02:43 -0400

    Spreading Objects

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -26,6 +26,19 @@ const removeExpense = ({ id } = {}) => ({
   id
 })
 
+// EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+})
+
+// SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+})
+
 // Expenses Reducer
 
 const expensesReducerDefaultState = []
@@ -37,8 +50,19 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
         ...state,
         action.expense
       ]
-      case 'REMOVE_EXPENSE':
-        return state.filter(({ id }) => id !== action.id)
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id)
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          }
+        } else {
+          return expense
+        }
+      })
     default:
       return state
   }
@@ -55,6 +79,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      }
     default:
       return state
   }
@@ -77,8 +106,10 @@ const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }))
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
 
-console.log(expenseOne)
+store.dispatch(setTextFilter('rent'))
+store.dispatch(setTextFilter())
 
 const demoState = {
   expenses: [{
